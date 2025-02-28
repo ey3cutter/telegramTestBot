15e68a281545c6ebf7ef329e2025048afa9024d6
ey3cutter <churros228@gmail.com>
2025-02-28 08:55:38 +0500

    added env

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"os"
+
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"log"
 	"telegramTestBot/bot"
@@ -8,9 +12,26 @@ import (
 )
 
 func main() {
-	db.InitDb("user=postgres password=12341234 dbname=gotgtest sslmode=disable")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-	var newBot = bot.NewBot("7792067052:AAFLPsJsLNyH4_75fdd9k_SNpE5Dh5vNcaA")
+	connStr := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_SSLMODE"),
+	)
+
+	db.InitDb(connStr)
+
+	botToken := os.Getenv("BOT_TOKEN")
+	if botToken == "" {
+		log.Fatal("Bot token not found")
+	}
+
+	var newBot = bot.NewBot(botToken)
 	log.Printf("Авторизован как %s", newBot.Bot.Self.UserName)
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
